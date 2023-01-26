576989e079ad1d561911a6e5a72dd57eafe1158e
LeonardoLopeso <l.lopes355@gmail.com>
2023-01-25 22:45:31 -0300

    Ajustes nos cards de projetos

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -93,7 +93,7 @@ const CardProject = styled.div`
   display: flex;
   gap: 1rem;
   padding: 6px;
-  min-height: 259px;
+  height: 259px;
   border-radius: 8px;
   background: rgba(217, 217, 217, 0.02);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -113,6 +113,7 @@ const ThumbProject = styled.div`
   img {
     width: 100%;
     height: 100%;
+    border-radius: 8px;
   }
 
   @media (max-width: 500px) {
